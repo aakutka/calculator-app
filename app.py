9b8f665b8861a923ecccc3da257a22ccab46d291
Aakash <singhalaakash22@gmail.com>
2024-06-17 13:14:26 -0400

    multiply

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 
 from cal import do_addition,do_sub
+from muliply import do_multipli
 
 def main():
     
     print("welcome to te ar")
     print("efc erfvd ervfd")
     
-    user_input=input("select the part system  1: add  2: subtrat")
+    user_input=input("select the part system  1: add  2: subtrat 3: multiply")
     a=int(input("A"))
     b=int(input("B"))
         
@@ -16,6 +17,9 @@ def main():
         
     elif user_input=="2":
         result=do_sub(a,b)
+        
+    elif user_input=="3":
+        result=do_multipli(a,b)
         
     print(result)
     
---
+++ muliply.py
@@ -0,0 +1,6 @@
+
+def do_multipli(a,b):
+    return a*b
+
+
+
